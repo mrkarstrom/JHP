9b016fd43f919d3868e680c8e217b0ed53dc64cb
Magnus Karström <magnus@yesweb.se>
2022-03-03 11:03:36 +0100

    Version 5 on the go

---
+++ ver4/app.js
@@ -0,0 +1,11 @@
+function openNav() {
+  let x = document.getElementById("topNav");
+  x.addEventListener("click", () => {
+    x.classList.toggle("responsive");
+  });
+
+  //   if (x.className === "topNav") {
+  //     x.className += " responsive";
+  //   } else {
+  //     x.className = "topNav";
+}
